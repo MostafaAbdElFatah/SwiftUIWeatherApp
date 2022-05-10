c33da522a2551a0484307debccb378c90d55a478
MostafaAbdElFatah <m.abdelfatah@gmail.com>
2022-05-10 04:50:31 +0200

    Initial Commit

---
+++ SwiftUIWeatherApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIWeatherApp
+//
+//  Created by Mostafa Abd ElFatah on 5/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIWeatherApp/SwiftUIWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWeatherAppApp.swift
+//  SwiftUIWeatherApp
+//
+//  Created by Mostafa Abd ElFatah on 5/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
